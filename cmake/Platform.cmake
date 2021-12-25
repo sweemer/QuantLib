b1b70d4b09894802b44f25fe1d839e6a1d08a54e
Jonathan Sweemer <sweemer@gmail.com>
2021-12-25 12:50:56 +0900

    Add support for nvc++

--- cmake/Platform.cmake
+++ cmake/Platform.cmake
@@ -22,4 +22,17 @@ if (MSVC)
     # Suppress warnings: "Prefer enum class over enum" (Enum.3)
 
     add_compile_options(/wd4267 /wd26812)
+
+elseif(CMAKE_CXX_COMPILER_ID STREQUAL "NVHPC")
+
+    add_compile_options(--preprocess)
+
+    # Display the error message number in any diagnostic messages that are generated. The option may be used to determine the error number to be used when overriding the severity of a diagnostic message.
+    add_compile_options(--display_error_number)
+
+    # warning #111-D: statement is unreachable
+    # warning #612-D: overloaded virtual function is only partially overridden in class
+    # warning #998-D: function is hidden -- virtual function override intended?
+    add_compile_options(--diag_suppress=111,612,998)
+
 endif()
